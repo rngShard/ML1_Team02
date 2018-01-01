59abb6cf7bc2ca600d4dfbd494123dc6c8859806
saurabh <saurabh.varsh@yahoo.com>
2018-01-01 16:36:16 +0100

    Use embeddings for tweet classification

--- src/text-classification-on-embedding/train.py
+++ src/text-classification-on-embedding/train.py
@@ -19,14 +19,14 @@ tf.flags.DEFINE_float("dev_sample_percentage", 0.1, "Percentage of the training 
 
 # Model Hyperparameters
 tf.flags.DEFINE_boolean("enable_word_embeddings", True, "Enable/disable the word embedding (default: True)")
-tf.flags.DEFINE_integer("embedding_dim", 128, "Dimensionality of character embedding (default: 128)")
+tf.flags.DEFINE_integer("embedding_dim", 200, "Dimensionality of character embedding (default: 128)")
 tf.flags.DEFINE_string("filter_sizes", "3,4,5", "Comma-separated filter sizes (default: '3,4,5')")
 tf.flags.DEFINE_integer("num_filters", 128, "Number of filters per filter size (default: 128)")
 tf.flags.DEFINE_float("dropout_keep_prob", 0.5, "Dropout keep probability (default: 0.5)")
 tf.flags.DEFINE_float("l2_reg_lambda", 0.0, "L2 regularization lambda (default: 0.0)")
 
 # Training parameters
-tf.flags.DEFINE_integer("batch_size", 64, "Batch Size (default: 64)")
+tf.flags.DEFINE_integer("batch_size", 32, "Batch Size (default: 64)")
 tf.flags.DEFINE_integer("num_epochs", 200, "Number of training epochs (default: 200)")
 tf.flags.DEFINE_integer("evaluate_every", 100, "Evaluate model on dev set after this many steps (default: 100)")
 tf.flags.DEFINE_integer("checkpoint_every", 100, "Save model after this many steps (default: 100)")
@@ -72,6 +72,11 @@ elif dataset_name == "localdata":
                                                      categories=cfg["datasets"][dataset_name]["categories"],
                                                      shuffle=cfg["datasets"][dataset_name]["shuffle"],
                                                      random_state=cfg["datasets"][dataset_name]["random_state"])
+
+elif dataset_name == "political_parties":
+    datasets = data_helpers.get_datasets_political_parties()
+
+
 x_text, y = data_helpers.load_data_labels(datasets)
 
 # Build vocabulary

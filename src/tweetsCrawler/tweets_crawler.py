e9eef1b4594d1ac0defbcca5c1b3f715aa7b9cf5
ibrahim_hussiy <ibrahim_hussiny@hotmail.com>
2018-01-12 21:24:51 +0100

    updated the train_data, and the data_gernerator script

--- src/tweetsCrawler/tweets_crawler.py
+++ src/tweetsCrawler/tweets_crawler.py
@@ -10,6 +10,7 @@ class TweetsCrawler:
     def __init__(self, input_file_path, output_csv_file_name):
         self.__input_file_path = input_file_path
         self.__output_csv_file_name = output_csv_file_name
+        self.__number_of_unique_tweets = 0
         #twitter app access tokens
         __consumer_key = "TcoDRSehIlGkWfxM0uXADVVKN"
         __consumer_secret = "u343Uv4Ta54zoHp5PALoXd5fYpSVhSfynUqqAQVwsn3cMtriPD"
@@ -24,21 +25,26 @@ class TweetsCrawler:
     def do_task(self):      
         politicians_data = self.__parse_input_file()
         remaining_number =len(politicians_data)
-        with open(self.__output_csv_file_name, 'wb') as out_csv:
+        with open(self.__output_csv_file_name, 'ab') as out_csv:
             csv_writer = csv.writer(out_csv)
             csv_writer.writerow(["politician_name", "party", "tweeted_at", "tweet"])
             for politician in politicians_data:
                 output_data = []
-                #sleep for .6s to avoid twitter api rate limit
-                time.sleep(.6)
+                
                 try:
+                    #sleep for .3s to avoid twitter api rate limit
+                    time.sleep(.3)
                     politician_tweets = self.__get_user_tweets(politician[-1])
                     for tweet in politician_tweets:
                         output_data.append([politician[0], politician[1], tweet[0], tweet[1]])
-                        #write output_data to output csv file 
-                        csv_writer.writerows(output_data)
-                except:
-                    print "error getting tweets for politician_name: %s" %   politician[0]      
+                        #to monitor how many tweets we have
+                        self.__number_of_unique_tweets +=1 
+                    #write output_data to output csv file 
+                    csv_writer.writerows(output_data)
+                    print "number of tweets : "+ str(self.__number_of_unique_tweets)    
+                except Exception, e:
+                    print "error getting tweets for politician_name: %s"  %   politician[0]
+                    print str(e)       
 
                 remaining_number-=1
                 print remaining_number
@@ -55,20 +61,38 @@ class TweetsCrawler:
                 #capture only twitter user name using Regex
                 tokens[-1] = match.group(1)
 
-                politicians_data.append(tokens[1:])
+                politicians_data.append(tokens)
         return politicians_data
 
-    #returns most recent 200 tweets of given user_name. 
+    #returns most recent 1000 tweets of given user_name. 
     #note : we can increase the number of tweets till 3240
     def __get_user_tweets(self,user_name):
         user_tweets = []
-        #for now we set the max number of tweets to get to 200
-        recent_tweets = self.__twitter_api.user_timeline(screen_name = user_name, count = 200)
-        for tweet in recent_tweets:
-            user_tweets.append([tweet.created_at, tweet.text.encode("utf-8")])
+
+        try:
+            #for now we set the max number of tweets to get to 200
+            recent_tweets = self.__twitter_api.user_timeline(screen_name = user_name, count = 200)
+            for tweet in recent_tweets:
+                user_tweets.append([tweet.created_at, tweet.text.encode("utf-8")]) 
+            #keep grabbing tweets until there are no tweets left to grab
+            for i in range(0, 4):
+                if len(recent_tweets) > 0:
+                    time.sleep(.3)
+                    #update the id of the oldest tweet less one
+                    oldest_id = recent_tweets[-1].id - 1
+                    #all subsiquent requests use the max_id param to prevent duplicates
+                    recent_tweets = self.__twitter_api.user_timeline(screen_name = user_name,count=200,max_id=oldest_id)
+
+                    for tweet in recent_tweets:
+                        user_tweets.append([tweet.created_at, tweet.text.encode("utf-8")])
+        except Exception,e:
+            print str(e)
+            return user_tweets            
+
+            
 
         return user_tweets
 
 
-tc = TweetsCrawler("data.txt","train.csv")
+tc = TweetsCrawler("cleaned_data2.txt","new_train.csv")
 tc.do_task()                
